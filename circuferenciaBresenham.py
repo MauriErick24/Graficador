f73a58abe512d1f70ad59b30b9664de624ab5f4b
samres27 <yosoyam4@gmail.com>
2023-04-28 15:33:38 -0400

    se añadio la funcionalidad de pintado por scanline
    hay que poner color del relleno y color del borde para implemntar

--- circuferenciaBresenham.py
+++ circuferenciaBresenham.py
@@ -3,9 +3,19 @@ import cv2
 import numpy
 class circuferencia:
     listaPuntosBorde=[]
+    listaPuntosRelleno=[]
+    matrizCircuferencia=[[]]
+    grosor=0
+    tamMax=0
+    separacion=0
+    finx=0
+    iniciox=0
     def __init__(self,pxi,pyi,pxf,pyf,grosor,separado):
         self.crearCirculoDiametro(pxi,pyi,pxf,pyf,grosor,separado)
-    def crearCirculoDiametro(self,pxi,pyi,pxf,pyf,grosor,separado):
+    def crearCirculoDiametro(self,pxi,pyi,pxf,pyf,groso,separado):
+        self.grosor=groso
+        self.separacion=separado
+        self.tamMax=max(pxi,pyi,pxf,pyf);
         falck=True
         diametro=math.sqrt(math.pow(pxf-pxi,2)+math.pow(pyf-pyi,2))
         radio = int(diametro/2)
@@ -15,23 +25,25 @@ class circuferencia:
         xi=0
         yi=radio
         contador=separado
+        self.iniciox=puntomediox-radio
+        self.finx=puntomediox+radio
         while(xi<=yi):
             if(contador==separado):
                 
-                for tlm in range(grosor):
+                for tlm in range(groso):
                     self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy-tlm))
                     self.listaPuntosBorde.append((yi+puntomediox-tlm,xi+puntomedioy))
                 xi=-xi
-                for tlm in range(grosor):
+                for tlm in range(groso):
                     self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy-tlm))
                     self.listaPuntosBorde.append((yi+puntomediox-tlm,xi+puntomedioy))
                 xi=-xi
                 yi=-yi
-                for tlm in range(grosor):
+                for tlm in range(groso):
                     self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy+tlm))
                     self.listaPuntosBorde.append((yi+puntomediox+tlm,xi+puntomedioy))
                 xi=-xi
-                for tlm in range(grosor):
+                for tlm in range(groso):
                     self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy+tlm))
                     self.listaPuntosBorde.append((yi+puntomediox+tlm,xi+puntomedioy))
                 xi=-xi
@@ -52,22 +64,58 @@ class circuferencia:
             
     def getPoints(self):
         return self.listaPuntosBorde
-    def pintar(self,tam):
-        color=[0,0,0]
+    def pintarScanLine(self):
+        #print("inicio",self.iniciox,self.inicioy)
+        #print("fin",self.finx,self.finy)
+        self.crearMatriz() 
+        puntosnegros=self.grosor-1
+        matrizPintar=self.matrizCircuferencia
+        tamano=len(matrizPintar)
+        for xi in range(tamano):
+            pintarlinea=False;
+            pintarBool=True
+            colorBorde=[0,0,0]
+            for yi in range(tamano):
+                if(not(xi-self.iniciox<=puntosnegros )and self.finx-xi>puntosnegros):
+                    if(numpy.array_equal(matrizPintar[xi][yi],colorBorde)):
+                        if(pintarBool):
+                            #este parte busca donde inicia el pintado y donde acaba, solo sirve para figuras regulares, y no va funcionar para otro tipo de figura 
+                             
+                            if(not(pintarlinea)):
+                                if(not(numpy.array_equal(matrizPintar[xi][yi+1],colorBorde))):pintarlinea=True
+                            else:    
+                                if(not(numpy.array_equal(matrizPintar[xi][yi-1],colorBorde))):
+                                    pintarlinea=False
+                                    pintarBool=False
+                        
+                    elif(pintarlinea ):
+                        self.listaPuntosRelleno.append((xi,yi))
+        #mostar 
+    def pintar(self):
+        self.crearMatriz() 
+        cv2.imshow('imgCircuferencia',self.matrizCircuferencia)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    def crearMatriz(self):
+        tam=self.tamMax
         tam=tam+10
-        matrizCircula=numpy.ones((tam,tam,3),numpy.uint8)*255
+        self.matrizCircuferencia=numpy.ones((tam,tam,3),numpy.uint8)*255
         
+        color=[0,0,0]# hay poner color primeramente 
         for pl in self.listaPuntosBorde:
             x,y=pl
             #print(x,y)
-            matrizCircula[x][y]=color
-            
-        cv2.imshow('imgCircuferencia',matrizCircula)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+            self.matrizCircuferencia[x][y]=color
+        color=[0,200,0]# el color despues de scanline
+        for pl in self.listaPuntosRelleno:
+            x,y=pl
+        
+            self.matrizCircuferencia[x][y]=color
     
                 
-nc=circuferencia(100,50,100,200,1,2)
+nc=circuferencia(100,50,100,250,7,4)
 print()
 #print(nc.getPoints())
-#nc.pintar(200)
+nc.pintar()
+nc.pintarScanLine();
+nc.pintar()
