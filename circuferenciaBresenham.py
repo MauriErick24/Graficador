251081ea8169e6e0e6f2fbecd84484afdb1f36bf
samres27 <yosoyam4@gmail.com>
2023-04-28 14:23:11 -0400

    se anadio la separacion de puntos

--- circuferenciaBresenham.py
+++ circuferenciaBresenham.py
@@ -1,9 +1,12 @@
 import math
+import cv2
+import numpy
 class circuferencia:
     listaPuntosBorde=[]
-    def __init__(self,pxi,pyi,pxf,pyf):
-        self.crearCirculoDiametro(pxi,pyi,pxf,pyf)
-    def crearCirculoDiametro(self,pxi,pyi,pxf,pyf):
+    def __init__(self,pxi,pyi,pxf,pyf,grosor,separado):
+        self.crearCirculoDiametro(pxi,pyi,pxf,pyf,grosor,separado)
+    def crearCirculoDiametro(self,pxi,pyi,pxf,pyf,grosor,separado):
+        falck=True
         diametro=math.sqrt(math.pow(pxf-pxi,2)+math.pow(pyf-pyi,2))
         radio = int(diametro/2)
         puntomediox=int((pxf+pxi)/2)
@@ -11,32 +14,60 @@ class circuferencia:
         puntomedioy=int((pyf+pyi)/2)
         xi=0
         yi=radio
+        contador=separado
         while(xi<=yi):
-            self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
-            self.listaPuntosBorde.append((yi+puntomedioy,xi+puntomediox))
-            xi=-xi
-            self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
-            self.listaPuntosBorde.append((yi+puntomedioy,xi+puntomediox))
-            xi=-xi
-            yi=-yi
-            self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
-            self.listaPuntosBorde.append((yi+puntomedioy,xi+puntomediox))
-            xi=-xi
-            self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
-            self.listaPuntosBorde.append((yi+puntomedioy,xi+puntomediox))
-            xi=-xi
-            yi=-yi
-            self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
-            self.listaPuntosBorde.append((yi+puntomedioy,xi+puntomediox))
+            if(contador==separado):
+                print(xi,yi)
+                self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
+                self.listaPuntosBorde.append((yi+puntomediox,xi+puntomedioy))
+                xi=-xi
+                self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
+                self.listaPuntosBorde.append((yi+puntomediox,xi+puntomedioy))
+                xi=-xi
+                yi=-yi
+                self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
+                self.listaPuntosBorde.append((yi+puntomediox,xi+puntomedioy))
+                xi=-xi
+                self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
+                self.listaPuntosBorde.append((yi+puntomediox,xi+puntomedioy))
+                xi=-xi
+                yi=-yi
+                self.listaPuntosBorde.append((xi+puntomediox,yi+puntomedioy))
+                self.listaPuntosBorde.append((yi+puntomediox,xi+puntomedioy))
+                contador=1
+            else:
+                contador+=1;
+                
             if(p0<0):
                
                 p0=p0+2*xi+2+1
                 xi=xi+1
             else:
                 p0=p0+2*xi+2+1-(2*yi-2)
-                xi=xi+1 
+                xi=xi+1
+                yi=yi-1
+            if(falck):
+                print(self.listaPuntosBorde) 
+                print(puntomediox,puntomedioy,radio)
+                falck=False
     def getPoints(self):
         return self.listaPuntosBorde
+    def pintar(self,tam):
+        color=[0,0,0]
+        tam=tam+10
+        matrizCircula=numpy.ones((tam,tam,3),numpy.uint8)*255
+        
+        for pl in self.listaPuntosBorde:
+            x,y=pl
+            print(x,y)
+            matrizCircula[x][y]=color
+            
+        cv2.imshow('imgCircuferencia',matrizCircula)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
                 
-nc=circuferencia(0,3,100,100)
-print(nc.getPoints())
+nc=circuferencia(100,50,100,100,1,1)
+print()
+#print(nc.getPoints())
+nc.pintar(200)
