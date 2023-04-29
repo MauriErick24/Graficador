c988aa6f261905796b81bff36c2e93739059682a
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2023-04-29 15:08:08 -0400

    disminuir tamanio agregado

--- linea.py
+++ linea.py
@@ -41,7 +41,7 @@ class Linea:
         # Devolver la matriz de coordenadas
         return coords[:, :idx]
 
-    def cambiarLongitud(self):
+    def aumentaLongitud(self):
         # Aumentar la longitud de la línea en 1 píxel en ambos extremos
         if self.x1 < self.x2:
             self.x1 -= 1
@@ -66,16 +66,48 @@ class Linea:
         # Actualizar la matriz de coordenadas con las nuevas coordenadas de la línea
         return self.dibujar()
 
+    def disminuirLongitud(self):
+        # Disminuir la longitud de la línea en 1 píxel en ambos extremos
+        if self.x1 < self.x2:
+            self.x1 += 1
+            self.x2 -= 1
+        else:
+            self.x1 -= 1
+            self.x2 += 1
+
+        if self.y1 < self.y2:
+            self.y1 += 1
+            self.y2 -= 1
+        else:
+            self.y1 -= 1
+            self.y2 += 1
+
+        # Actualizar los atributos con las nuevas coordenadas de los extremos de la línea
+        self.x1 = max(0, self.x1)
+        self.y1 = max(0, self.y1)
+        self.x2 = max(0, self.x2)
+        self.y2 = max(0, self.y2)
+
+        # Actualizar la matriz de coordenadas con las nuevas coordenadas de la línea
+        return self.dibujar()
 
 #matriz de dibujar
 linea = Linea(50,20,145,100)
 coordenadasDibujo=linea.dibujar()
 print(coordenadasDibujo)
-#matriz que aumenta el tamanio cada que se llama al metodo cambiarLongitud()
-print("Coordenadas modificadas para aumentar la longitud")
-nuevasCoordenadas = linea.cambiarLongitud()
+
+ #matriz que aumenta el tamanio cada que se llama al metodo aumentarLongitud()
+""" print("Coordenadas modificadas para aumentar la longitud")
+nuevasCoordenadas = linea.aumentaLongitud()
 print(nuevasCoordenadas)
-nuevasCoordenadas = linea.cambiarLongitud()
+nuevasCoordenadas = linea.aumentaLongitud()
+print(nuevasCoordenadas) """ 
+
+#matriz que reduce el tamanio cada que se llama al metodo disminuirLongitud()
+print("Coordenadas modificadas para disminuir la longitud")
+nuevasCoordenadas = linea.disminuirLongitud()
+print(nuevasCoordenadas)
+nuevasCoordenadas = linea.disminuirLongitud()
 print(nuevasCoordenadas)
 
 
